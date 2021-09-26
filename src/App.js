fb06934cd46d784c2cc87ebf348d3b6201c79ac2
Richard Li <richardlidefa@gmail.com>
2021-09-26 21:14:26 +0800

    Richard changes

--- src/App.js
+++ src/App.js
@@ -37,26 +37,53 @@ function App() {
     try {
       let dataSet1 = await HDBCarparkInfo();
       console.log("dataSet1:", dataSet1);
+
       let dataSet2 = await CSVArray();
       console.log("dataSet2:", dataSet2);
 
-      const filteredInfoArray = dataSet2.filter((carparkInfo2) => {
+      function isInDataSet1(carpark) {
         for (let i = 0; i < dataSet1.length; i++) {
-          if (
-            dataSet1[i].carpark_number === carparkInfo2[i].car_park_no
-          )
-            return carparkInfo2;
-        };
-      });
+          if (dataSet1[i].carpark_number === carpark) return true;
+        }
+        return false;
+      }
+
+      let finalArray = [] // Contain all carparks both x, y and number
+
+      function combineCarparkWith1(carparkFromData2) {
+        for (let i = 0; i < dataSet1.length; i++) {
+          if (dataSet1[i].carpark_number === carparkFromData2.car_park_no) {
+            const combinedCapark = {
+              ...carparkFromData2,
+              total_lots: dataSet1[i].total_lots
+            }
+            return combinedCapark
+          }
+          return null
+        }
+      }
+
+      for (const carpark in dataSet2) {
+        const result = combineCarparkWith1(carpark)
+        if (result !== null) finalArray.push(result)
+      }
+
+      console.log(finalArray)
+      // const filteredInfoArray = dataSet2.filter((carparkInfo2) => {
+      //   return isInDataSet1(carparkInfo2.car_park_no)
+      // });
+      // console.log("filtered: ", filteredInfoArray)
     }
     catch (err) {
       console.log(err);
     }
   }
 
+  const something = finalHDBCarparkInfo();
 
   return (
     <div>
+      Hello
     </div>
   );
 }
